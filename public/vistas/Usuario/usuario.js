eaf45b6dded3188c92b89064f44e929450927d8c
Tania_Bonilla <t.a.b.a.tania@hotmail.com>
2020-04-30 23:17:05 -0600

    xd

--- public/vistas/Usuario/usuario.js
+++ public/vistas/Usuario/usuario.js
@@ -1,28 +1,8 @@
-var appusuario = new Vue({
-    el:'#frm-usuario',
-    data:{
-        usuario:{
-            idUsuario    :0,
-            accion       :'nuevo',
-            nombre       :'',
-            apellido     :'',
-            correo       :'',
-            genero       :'',
-            msg          :''
-            }
-        },
-        methods:{
-            guardarUsuario:function(){
-                fetch(`private/Modulos/Usuario/procesos.php?proceso=recibirDatos&usuario=${JSON.stringify(this.usuario)}`).then(resp => resp.json()).then(resp=>{
-                    this.usuario.msg = resp.msg;
-                    this.usuario.idUsuario = 0;
-                    this.usuario.nombre = '';
-                    this.usuario.apellido = '';
-                    this.usuario.correo = '';
-                    this.usuario.genero = '';
-                    this.usuario.accion = 'nuevo';
-                    appBuscarUsuario.buscarUsuario();
-                 });    
-        }
-        }
-});
+$(function () {
+    $('.min-chart#chart-sales').easyPieChart({
+      barColor: "#4caf50",
+      onStep: function (from, to, percent) {
+        $(this.el).find('.percent').text(Math.round(percent));
+      }
+    });
+  });
